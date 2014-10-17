ff1f492a5135a6bbf73a87157126b04cfe5f8d77
Young Z. Wang <wzystal@gmail.com>
2014-10-17 18:00:18 +0800

    代码更新

--- src/com/baidu/mediarecorder/util/CameraHelper.java
+++ src/com/baidu/mediarecorder/util/CameraHelper.java
@@ -4,13 +4,25 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
+import android.graphics.Rect;
 import android.hardware.Camera;
 import android.hardware.Camera.Size;
 import android.util.Log;
 
+/**
+ * CameraHelper
+ * @author wzystal@gmail.com
+ * 2014-10-14
+ */
 public class CameraHelper {
 
-	// 根据预设宽高获取最合适的相机支持尺寸
+	/**
+	 * 根据预设宽高获取匹配的相机分辨率,若没有则返回中间值
+	 * @param camera 相机
+	 * @param width 预设宽度
+	 * @param height 预设高度
+	 * @return
+	 */
 	public static Size getOptimalPreviewSize(Camera camera, int width,
 			int height) {
 		Size previewSize = null;
@@ -45,5 +57,39 @@ public class CameraHelper {
 		}
 	}
 	
-	// 摄像头对焦
+	/**
+	 * 将屏幕坐标系转化成对焦坐标系,返回要对焦的矩形框
+	 * @param x 横坐标
+	 * @param y 纵坐标
+	 * @param w 相机宽度
+	 * @param h 相机高度
+	 * @param areaSize 对焦区域大小
+	 * @return
+	 */
+	public static Rect getFocusArea(int x, int y, int w, int h, int areaSize) {
+		int centerX = x / w * 2000 - 1000;
+		int centerY = y / h * 2000 - 1000;
+		int left = clamp(centerX - areaSize / 2, -1000, 1000);
+		int right = clamp(left + areaSize, -1000, 1000);
+		int top = clamp(centerY - areaSize / 2, -1000, 1000);
+		int bottom = clamp(top + areaSize, -1000, 1000);
+		return new Rect(left, top, right, bottom);
+	}
+
+	/**
+	 * 限定x取值范围为[min,max]
+	 * @param x
+	 * @param min
+	 * @param max
+	 * @return
+	 */
+	public static int clamp(int x, int min, int max) {
+		if (x > max) {
+			return max;
+		}
+		if (x < min) {
+			return min;
+		}
+		return x;
+	}
 }
